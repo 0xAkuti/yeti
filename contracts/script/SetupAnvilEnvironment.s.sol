3248baf33760138e7bf642d2e0033fcf20bb495c
Akuti <40723201+0xAkuti@users.noreply.github.com>
2025-07-28 19:55:52 +0000

    impl: deploy predicate in setup

--- contracts/script/SetupAnvilEnvironment.s.sol
+++ contracts/script/SetupAnvilEnvironment.s.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
 import {WebhookOracle} from "../src/WebhookOracle.sol";
+import {WebhookPredicate} from "../src/WebhookPredicate.sol";
 
 contract DeployWebhookOracle is Script {
     address constant TEE_SIMULATOR = 0xf3D21A3A689AD889541d993A51e3109aC3E36c12;
@@ -10,12 +11,15 @@ contract DeployWebhookOracle is Script {
     function run() public {
         vm.startBroadcast();
 
-        payable(TEE_SIMULATOR).transfer(10 ether);
+        payable(TEE_SIMULATOR).transfer(1 ether);
         
         WebhookOracle oracle = new WebhookOracle();
         oracle.addAuthorizedSubmitter(TEE_SIMULATOR);
+        WebhookPredicate predicate = new WebhookPredicate(address(oracle), 300);
         
         console.log("WebhookOracle deployed to:", address(oracle));
+        console.log("WebhookPredicate deployed to:", address(predicate));
+
         console.log("TEE account funded:", TEE_SIMULATOR);
         console.log("TEE balance:", TEE_SIMULATOR.balance / 1e18, "ETH");
         
